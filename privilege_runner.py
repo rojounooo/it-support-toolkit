1363798749fe44dfdd862aa0955fd7c07f3af606
rojounooo <rojounooo@gmail.com>
2026-02-07 17:00:23 +0000

    updated runner to have commands for admin and non admin

--- privilege_runner.py
+++ privilege_runner.py
@@ -13,12 +13,26 @@ def is_admin():
     else:
         return os.geteuid() == 0
 
-def run_command(command, admin_required=False, dry_run=False):
+def run_command(command, dry_run=False):
+    if dry_run:
+        print("\nDry run mode: the command that would be executed is:")
+        print(" ".join(command))
+        return None
+
+    return subprocess.run(
+        command,
+        stdin=sys.stdin,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        text=True
+    )
+
+def run_admin_command(command, dry_run=False):
     system = platform.system()
     user_is_admin = is_admin()
 
     if system == "Windows":
-        if admin_required and not user_is_admin:
+        if not user_is_admin:
             argument_list = " ".join(command)
             command_to_run = [
                 "powershell.exe",
@@ -28,23 +42,20 @@ def run_command(command, admin_required=False, dry_run=False):
         else:
             command_to_run = command
     else:
-        command_to_run = command[:]
-        if admin_required and not user_is_admin:
-            command_to_run.insert(0, "sudo")
+        if not user_is_admin:
+            command_to_run = ["sudo", *command]
+        else:
+            command_to_run = command
 
     if dry_run:
         print("\nDry run mode: the command that would be executed is:")
         print(" ".join(command_to_run))
         return None
 
-    try:
-        return subprocess.run(
-            command_to_run,
-            stdin=sys.stdin,
-            stdout=sys.stdout,
-            stderr=sys.stderr,
-            text=True
-        )
-    except Exception as e:
-        print(f"Failed to execute command: {e}")
-        return None
+    return subprocess.run(
+        command_to_run,
+        stdin=sys.stdin,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        text=True
+    )
