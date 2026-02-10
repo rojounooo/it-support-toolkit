2a5c29f6b42d7efb88b5779a34aed6963c698492
rojounooo <rojounooo@gmail.com>
2026-02-10 20:13:35 +0000

    base logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,19 @@
+# Import modules
+import logging
+import platform
+import getpass
+from datetime import datetime
 
+# Define log file name (creates in the same directory as the script)
+LOG_FILE = "it_support_toolkit.log"
+
+
+logger = logging.getLogger("toolkit_logs") # Creates a logger instance
+logger.setLevel(logging.INFO) # Sets the minimum level of messages to log
+
+
+# Create a file handler
+handler = logging.FileHandler(LOG_FILE)
+
+
+    
